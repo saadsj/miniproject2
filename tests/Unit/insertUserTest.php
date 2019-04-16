29ec534b61075cc5a74e4afddad66deff11ec6a8
saadsj <47307940+saadsj@users.noreply.github.com>
2019-04-16 05:47:34 -0400

    Create a unit test to check if a car's year is an integer.

--- tests/Unit/insertUserTest.php
+++ tests/Unit/insertUserTest.php
@@ -18,7 +18,7 @@ class insertUserTest extends TestCase
     {
         $user= new User();
         $user->name = 'J.J.Johnson';
-        $user->email = 'bdggh@gzjzd.com';
+        $user->email = 'bdggh@gzjffzd.com';
         $user->password = '000000000';
         $this->assertTrue($user->save());
 //        $this->assertTrue(true);
